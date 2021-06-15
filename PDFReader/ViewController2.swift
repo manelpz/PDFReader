d51728c12f444600bcf0961cf2ae835f92aca3d7
emmanuel lopez <emmanuel.lopez.g@icloud.com>
2021-06-15 11:05:28 -0500

    adding load pdf on viewController

--- PDFReader/ViewController2.swift
+++ PDFReader/ViewController2.swift
@@ -6,20 +6,30 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController2: UIViewController {
 
-    @IBOutlet var Titulo: UILabel!
+    @IBOutlet var vistaWeb: WKWebView!
     var nombrePDFRecibido:String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        Titulo.text = nombrePDFRecibido!
+        mostrarPDF()
+        //Titulo.text = nombrePDFRecibido!
         // Do any additional setup after loading the view.
     }
     
-
+    func mostrarPDF(){
+        //1: direccion del archivo pdf
+        let direccionPDF = URL(fileURLWithPath: Bundle.main.path(forResource: nombrePDFRecibido, ofType: "pdf", inDirectory: "PDF")!)
+        //2: trasformar archivo pdf a data
+        let datosPDF = try? Data(contentsOf: direccionPDF)
+        
+        //3: mostrar datos en la vista web
+        vistaWeb.load(datosPDF!, mimeType: "application/pdf", characterEncodingName: "utf-8", baseURL: direccionPDF)
+        
+    }
     /*
     // MARK: - Navigation
 
