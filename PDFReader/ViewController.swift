cd20fbdc1601d54f759a0bbc7c0651dfe4853979
emmanuel lopez <emmanuel.lopez.g@icloud.com>
2021-06-14 23:36:24 -0500

    adding recycling views

--- PDFReader/ViewController.swift
+++ PDFReader/ViewController.swift
@@ -9,27 +9,34 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+     var contenidoCeldas = ["Juan", "Emmanuel", "diego", "raul", "rosa"]
+    
     @IBOutlet var tabla: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        //Adding Relation bethween table with viewController, 2 ways to do this
         tabla.dataSource = self
         tabla.delegate = self
     }
 
+    //Implementing number of rows
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 2
+        return contenidoCeldas.count
     }
     
+    //field the table
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        let celda = UITableViewCell()
-        celda.textLabel?.text = "Emmanuel"
+        //recycling rows
+        let celda = UITableViewCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: "Cell")
+        celda.textLabel?.text = contenidoCeldas[indexPath.row]
         
         return celda
     }
+    
     
     
 }
