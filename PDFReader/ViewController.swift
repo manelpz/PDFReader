38dc5ce505fc427103cb5d70ecc9efef1e676606
emmanuel lopez <emmanuel.lopez.g@icloud.com>
2021-06-14 22:31:38 -0500

    adding func numberOfRowsInSection

--- PDFReader/ViewController.swift
+++ PDFReader/ViewController.swift
@@ -7,13 +7,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    
 }
 
