a56e0632f6ea82bcfef72a64f76b6bfae36f0a7a
emmanuel lopez <emmanuel.lopez.g@icloud.com>
2021-06-14 23:05:39 -0500

    adding relation Navigation controller ViewController

--- PDFReader/ViewController.swift
+++ PDFReader/ViewController.swift
@@ -9,13 +9,26 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet var tabla: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        tabla.dataSource = self
+        tabla.delegate = self
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let celda = UITableViewCell()
+        celda.textLabel?.text = "Emmanuel"
+        
+        return celda
     }
     
     
